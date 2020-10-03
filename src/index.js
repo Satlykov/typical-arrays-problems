e44034d4784091231fc040b5e57fb08a5e13af62
Satlykov <70792214+Satlykov@users.noreply.github.com>
2020-10-03 11:59:27 +0500

    feat:  implemented the required functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    } else {
+        array.sort((a, b) => a - b);
+        console.log(array[0]);
+        return array[0];
+    }
+};
+exports.max = function max(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    } else {
+        array.sort((a, b) => a - b);
+        let indexLast = array.length - 1;
+        console.log(array[indexLast]);
+        return array[indexLast];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length === 0) {
+        return 0;
+    } else {
+        const summ = (accumulator, currentValue) => accumulator + currentValue;
+        let avg = array.reduce(summ) / array.length;
+        console.log(avg);
+        return avg;
+    }
+};
